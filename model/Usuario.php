b21308816a41ba38f88c0768aeea45f45125dfb1
Dee Fialho <deefialho@gmail.com>
2021-10-02 16:18:03 -0300

    organização pro trabalho 2 + paginas de login, php

---
+++ model/Usuario.php
@@ -0,0 +1,21 @@
+<?php
+  
+
+    $username = $_POST['username'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	
+	// Database connection
+	$conn = new mysqli('localhost','root','password','aperture');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into cadastro(username, email, password) values(?, ?, ?)");
+		$stmt->bind_param("sss", $username, $email, $password);
+		$execval = $stmt->execute();
+		
+		$stmt->close();
+		$conn->close();
+	}
+    
